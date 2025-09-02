d6b770dea86ccd74e222e6314cb1bd60522314fa
Aayush Kukreja <113628608+Aayush6377@users.noreply.github.com>
2025-09-02 16:54:56 +0530

    Update foodApi.jsx

--- admin/src/API/foodApi.jsx
+++ admin/src/API/foodApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:3000";
+export const url = "https://tomato-backend-6jff.onrender.com";
 
 export const api = axios.create({
     baseURL: url
